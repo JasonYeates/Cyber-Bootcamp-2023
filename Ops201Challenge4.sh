3d2b69c040750fbc44923559a84168b89f75d3da
Jason Yeates <jasony941@gmail.com>
2023-11-09 17:24:20 -0700

    update

---
+++ Ops201Challenge4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Lets create a function in bash that adds two number together
+# Stretch goal can you do subtraction, multipliaction or division
+# You will need to declare two variables
+echo "Enter two numbers to add"
+read num1
+read num2
+total=$(($num1 + $num2))
+echo $total
+
+
