5bc0e3f6e17d4898020b96493899b6802d9426f7
JasonYeates <146894705+JasonYeates@users.noreply.github.com>
2023-11-21 18:47:57 -0700

    Update Ops201Challenge9.sh

--- Ops201Challenge9.sh
+++ Ops201Challenge9.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Lets create an until loop that takes a variable and adds 1 till it reachs 10
-# Have the script echo out what are current number is
+# Let's create an until loop that takes a variable and adds 1 till it reaches 10
+# Have the script echo out what our current number is
 
 x=1
-until [ $x = 10 ]
+until [ $x = 11 ]
 do
     echo "x is $x"
     ((x++))
