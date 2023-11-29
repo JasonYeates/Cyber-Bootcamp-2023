c0bbdfab6d9bb69c20beed7a47bdf870caa3df5b
Jason Yeates <jasony941@gmail.com>
2023-11-28 17:15:42 -0700

    update

---
+++ Ops201Challenge11.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Today we are going to use a case statment instead of a conditional
+# Have the program ask how your day is and echo out a response for good or bad
+# Case statment format is a little different so please look up how this would be formated
+
+echo "How is your day?"
+read response
+case "$response" in
+    "good")
+        echo "Awesome!!!!"
+        ;;
+    "bad")
+        echo "Not awesome."
+        ;;
+    *)
+        echo "Invalid response"
+        ;;
+esac
